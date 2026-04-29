281b2e6860c920a566be34fefd5497374a4cbbe4
VIJAYASRI <2483370.cognizant.com>
2026-04-29 23:14:52 +0530

    pushing the reporting service

--- reporting-service/src/main/java/com/cts/report/service/SLAService.java
+++ reporting-service/src/main/java/com/cts/report/service/SLAService.java
@@ -12,7 +12,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
